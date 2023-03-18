a6144dd3bff2e138850a26ef39245b43dcb0eb57
Viih Neris <viihneris049@gmail.com>
2023-03-17 21:30:24 -0300

    Auth, User and Bookmarks e2e tests part. 1

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { Controller, Get, Patch, Body, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
   @UseGuards(JwtGuard)
   @Get('me')
   getMe(@GetUser() user: User, @GetUser('email') email: string) {
@@ -12,5 +15,7 @@ export class UserController {
   }
 
   @Patch()
-  editUser() {}
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
+  }
 }
